01d78723a74c6a3dd40284a863b783ad487cbcdd
catacoman1 <catalin.coman@student.upt.ro>
2023-10-06 16:21:33 +0300

    Added and updated Swagger Documentation

--- src/main/java/com/RestAPIdb/RestApiDB/RestApiDbApplication.java
+++ src/main/java/com/RestAPIdb/RestApiDB/RestApiDbApplication.java
@@ -1,9 +1,25 @@
 package com.RestAPIdb.RestApiDB;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot REST API Documentation",
+				description = "Spring Boot REST API Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Catalin",
+						email = "catacoman11@gmail.com"
+				)
+
+
+		)
+)
 public class RestApiDbApplication {
 
 	public static void main(String[] args) {
